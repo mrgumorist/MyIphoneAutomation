2cc9ae92cccea4269a3bccb1555bb22e7c5607d3
Andrew Smirnow <mrgumorist@gmail.com>
2021-08-06 14:11:27 +0300

    Add project files.

---
+++ iPhoneWorker/Models/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace iPhoneWorker
+{
+    public class Phone
+    {
+        private string _phoneIP;
+        private string _phoneSSHLogin;
+        private string _phoneSSHPass;
+
+        public Phone(string phoneIP, string phoneSSHLogin, string phoneSSHPass)
+        {
+            _phoneIP = phoneIP;
+            _phoneSSHLogin = phoneSSHLogin;
+            _phoneSSHPass = phoneSSHPass;
+        }
+    }
+}
